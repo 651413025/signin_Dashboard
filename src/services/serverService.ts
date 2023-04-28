5259d05936c6ab15298ccf2f9a3e1db4912f82bb
651413025 <651413025@crru.ac.th>
2023-04-28 16:56:34 +0700

    first commit

---
+++ src/services/serverService.ts
@@ -0,0 +1,29 @@
+import { Customer } from '@/models/customer.model'
+import { Product } from '@/models/product.model'
+import { SignIn } from '@/models/signin.model'
+import httpClient from '@/utils/httpClient'
+
+type SignInProps = {
+  username: string
+  password: string
+}
+// AUTHENTICATION
+export const signIn = async (credential: SignInProps): Promise<SignIn> => {
+  const { data: response } = await httpClient.post<SignIn>('/auth', credential)
+  return response
+}
+
+export const signOut = async (): Promise<void> => {
+  await httpClient.post('auth/logout', null)
+}
+
+// PRODUCT
+export const fetchProducts = async (): Promise<Product> => {
+  const { data: response } = await httpClient.get<Product>('/products')
+  return response
+}
+// CUSTOMER
+export const fetchCustomers = async (): Promise<Customer> => {
+  const { data: response } = await httpClient.get<Customer>('/customers')
+  return response
+}
---
+++ src/utils/common.ts
@@ -0,0 +1,5 @@
+export const imageUrl = (name: string) => {
+    return import.meta.env.VITE_PUBLIC_BASE_IMAGE_URL + '/' + name
+  }
+  
+  
